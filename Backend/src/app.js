51fbd6e5148be21a3a50b962869229a6f46e64f9
Ishan Bhardwaj <ishanbhardwaj177@gmail.com>
2026-02-17 19:43:46 +0530

    connected to imageKit

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,11 +1,15 @@
 const express = require('express');
-const cookieParser = require("cookie-parser")
+const cookieParser = require("cookie-parser");
 const authRouter = require('../src/routes/auth.routes');
+const postRouter = require('../src/routes/post.routes');
 
 const app = express();
 
 app.use(express.json());
-app.use(cookieParser())
+app.use(cookieParser());
 
-app.use("/api/auth", authRouter)
+app.use("/api/auth", authRouter);
+app.use("/api/post", postRouter);
+
+
 module.exports = app;
---
+++ Backend/src/routes/post.routes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const postController = require('../controllers/post.controller');
+const multer = require('multer');
+const storage = multer.memoryStorage()
+const upload = multer({ storage: storage });
+
+const postRouter = express.Router();
+
+postRouter.post('/',upload.single('image'),postController.createPostController);
+
+module.exports = postRouter
